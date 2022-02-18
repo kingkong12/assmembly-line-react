123839b6d69ceadb28de2948cae0762719f2e423
mithil9 <mithil.vasani@nium.com>
2022-02-18 17:23:41 +0530

    (wip): state initialization

--- src/pages/AssemblyLine.tsx
+++ src/pages/AssemblyLine.tsx
@@ -1,11 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { AssemblyLineStagesTS } from 'smeui-interfaces/constants'
 
 interface AssemblyLineTS {
   stages: AssemblyLineStagesTS
 }
 
+interface StateWithIdsTS {
+  [a: string]: Array<string>
+}
+
 const AssemblyLine: FC<AssemblyLineTS> = ({ stages }) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const [state, setState] = useState<StateWithIdsTS | {}>({})
+
+  useEffect(() => {
+    const convertedStagesToState = stages.reduce((ac, a) => ({ ...ac, [a]: [] }), {})
+    setState(convertedStagesToState)
+  }, [stages])
+
+  console.log('state', state)
   return <div>Assembly line</div>
 }
 
