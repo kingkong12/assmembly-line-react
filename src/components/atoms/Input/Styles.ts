ee2f3a144bf933c1624488e694d1c97a0efdbefa
mithil9 <mithil.vasani@nium.com>
2022-02-18 18:19:40 +0530

    functional inout bar whic updates central state

---
+++ src/components/atoms/Input/Styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+export const BaseInput = styled.input`
+  padding: 8px 8px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  box-sizing: border-box;
+  width: 300px;
+`
