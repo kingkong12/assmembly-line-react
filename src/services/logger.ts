cc0a0fd08459fae4ab2820c0a17004e12983100f
mithil9 <mithil.vasani@nium.com>
2022-02-22 13:31:52 +0530

    disbaling loggers file

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,28 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import Debug from 'debug'
-import { ServicesLogger } from 'smeui-interfaces/services'
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import Debug from 'debug'
+// import { ServicesLogger } from 'smeui-interfaces/services'
 
-const debugLogger = (
-  namespace: string,
-  logLevel: string,
-  enabledLogger: boolean,
-  ...msgs: any[]
-): void => {
-  const debugLogger = Debug(`${namespace}:${logLevel}`)
+// const debugLogger = (
+//   namespace: string,
+//   logLevel: string,
+//   enabledLogger: boolean,
+//   ...msgs: any[]
+// ): void => {
+//   const debugLogger = Debug(`${namespace}:${logLevel}`)
 
-  if (enabledLogger) Debug.enabled(`${namespace}:${logLevel}:*`)
+//   if (enabledLogger) Debug.enabled(`${namespace}:${logLevel}:*`)
 
-  // eslint-disable-next-line prefer-spread
-  return debugLogger.apply(undefined, ['%O', ...msgs])
-}
+//   // eslint-disable-next-line prefer-spread
+//   return debugLogger.apply(undefined, ['%O', ...msgs])
+// }
 
-export const Logger = (namespace: string): ServicesLogger => {
-  return {
-    silly: (...msgs: any[]): void => debugLogger(namespace, 'silly', false, ...msgs),
-    verbose: (...msgs: any[]): void => debugLogger(namespace, 'verbose', false, ...msgs),
-    debug: (...msgs: any[]): void => debugLogger(namespace, 'debug', false, ...msgs),
-    info: (...msgs: any[]): void => debugLogger(namespace, 'info', false, ...msgs),
-    warn: (...msgs: any[]): void => debugLogger(namespace, 'warn', false, ...msgs),
-    error: (...msgs: any[]): void => debugLogger(namespace, 'error', true, ...msgs),
-  }
-}
+// export const Logger = (namespace: string): ServicesLogger => {
+//   return {
+//     silly: (...msgs: any[]): void => debugLogger(namespace, 'silly', false, ...msgs),
+//     verbose: (...msgs: any[]): void => debugLogger(namespace, 'verbose', false, ...msgs),
+//     debug: (...msgs: any[]): void => debugLogger(namespace, 'debug', false, ...msgs),
+//     info: (...msgs: any[]): void => debugLogger(namespace, 'info', false, ...msgs),
+//     warn: (...msgs: any[]): void => debugLogger(namespace, 'warn', false, ...msgs),
+//     error: (...msgs: any[]): void => debugLogger(namespace, 'error', true, ...msgs),
+//   }
+// }
